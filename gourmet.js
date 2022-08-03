5f212bbe09498be89500fee062b66d58337ba48c
G184642021 <218464@st.takushoku-u.ac.jp>
2022-08-03 22:47:58 +0900

    1

--- gourmet.js
+++ gourmet.js
@@ -236,6 +236,59 @@ function showResult(resp) {
   data = JSON.parse(data);
  }
 
+ let table = document.querySelector('table#tb');
+
+  let tdb = document.createElement('table');
+
+  tdb.id = 'db';
+
+  table.insertAdjacentElement('afterend',tdb);
+
+  let trdb = document.createElement('tr');
+  let throws = document.createElement('th');
+
+  tdb.insertAdjacentElement('beforeend',trdb);
+
+  throws.textContent = '検索結果';
+
+  trdb.insertAdjacentElement('beforeend',throws)
+  throws.setAttribute('rowspan','60');
+  throws.classList.add('v-align');
+  throws.classList.add('bg-b');
+
+  let datalist;
+  if (data.list.g1) {
+    datalist = data.list.g1;
+  } else {
+    datalist = data.list.e1;
+  }
+
+
+  for(let d of datalist){
+
+    let tdtitle = document.createElement('td');
+    let thtime = document.createElement('th');
+    let tr = document.createElement('tr');
+    let tr1 = document.createElement('tr');
+    let count = 0;
+
+    if(count === 0){
+      trdb.insertAdjacentElement('afterend',tr);
+    }
+
+    thtime.textContent = d.start_time;
+    tdtitle.textContent = d.title;
+
+    tr.insertAdjacentElement('beforeend',thtime);
+
+    tr.insertAdjacentElement('afterend',tr1);
+
+    tr1.insertAdjacentElement('beforeend',tdtitle);
+
+    tr1.classList.add('tr');
+
+  }
+
  // data をコンソールに出力
  console.log(data);
 
